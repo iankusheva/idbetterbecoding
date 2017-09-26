ac483eedea7ba6f10e9616170f9cef9ca6094f82
iankusheva <irene.ankusheva@gmail.com>
2017-09-26 22:13:32 +0300

    ok

--- sample.py
+++ sample.py
@@ -1,6 +1,7 @@
 def main():
-	print('That\'s all I am capable of in Python, pam pam')
+    print('That\'s all I am capable of in Python, pam pam')
+    print('Just kidding, I can also print hearts <3<3<3')
 
 
 if __name__ == '__main__':
-	main()
+    main()

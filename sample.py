c3e5007106881ced179700eb3b46f08890479d0c
iankusheva <irene.ankusheva@gmail.com>
2017-10-03 22:03:16 +0300

    lalala

--- sample.py
+++ sample.py
@@ -1,6 +1,23 @@
-def main():
-    print('That\'s all I am capable of in Python, pam pam')
-    print('Just kidding, I can also print hearts <3<3<3')
+import click
+import os
+import sqlite3
+
+
+def manipulate_tables(dbname):
+    running_dir = os.path.dirname(os.path.abspath(__file__))
+    if os.path.exists(os.path.join(running_dir, '{}.db'.format(dbname))):
+        print('Database {}.db already exists'.format(dbname))
+        return
+    conn = sqlite3.connect('{}.db'.format(dbname))
+    cursor = conn.cursor()
+    cursor.execute('CREATE TABLE table1 ('
+                   'userid integer PRIMARY KEY, username text)')
+
+
+@click.command()
+@click.option('--dbname', prompt='Enter database name')
+def main(dbname):
+    manipulate_tables(dbname)
 
 
 if __name__ == '__main__':

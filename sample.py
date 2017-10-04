d390828ac2ea693ccd837f2f63a5184bbeb8fa6b
iankusheva <irene.ankusheva@gmail.com>
2017-10-04 20:15:28 +0300

    fixed cli and db path

--- sample.py
+++ sample.py
@@ -3,21 +3,22 @@ import os
 import sqlite3
 
 
+@click.command()
+@click.option('--dbname', prompt='Enter database name')
 def manipulate_tables(dbname):
     running_dir = os.path.dirname(os.path.abspath(__file__))
-    if os.path.exists(os.path.join(running_dir, '{}.db'.format(dbname))):
+    path_to_db = os.path.join(running_dir, '{}.db'.format(dbname))
+    if os.path.exists(path_to_db):
         print('Database {}.db already exists'.format(dbname))
         return
-    conn = sqlite3.connect('{}.db'.format(dbname))
+    conn = sqlite3.connect(path_to_db)
     cursor = conn.cursor()
     cursor.execute('CREATE TABLE table1 ('
                    'userid integer PRIMARY KEY, username text)')
 
 
-@click.command()
-@click.option('--dbname', prompt='Enter database name')
-def main(dbname):
-    manipulate_tables(dbname)
+def main():
+    manipulate_tables()
 
 
 if __name__ == '__main__':

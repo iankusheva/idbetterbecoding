234dd8f1cebe9994e279ad7e3fc083e2f57dc8a1
Irene <irene.ankusheva@gmail.com>
2017-10-07 19:41:41 +0300

    task1
    * lalala
    
    * fixed cli and db path
    
    * fixed Pipfile

--- sample.py
+++ sample.py
@@ -1,6 +1,24 @@
+import click
+import os
+import sqlite3
+
+
+@click.command()
+@click.option('--dbname', prompt='Enter database name')
+def manipulate_tables(dbname):
+    running_dir = os.path.dirname(os.path.abspath(__file__))
+    path_to_db = os.path.join(running_dir, '{}.db'.format(dbname))
+    if os.path.exists(path_to_db):
+        print('Database {}.db already exists'.format(dbname))
+        return
+    conn = sqlite3.connect(path_to_db)
+    cursor = conn.cursor()
+    cursor.execute('CREATE TABLE table1 ('
+                   'userid integer PRIMARY KEY, username text)')
+
+
 def main():
-    print('That\'s all I am capable of in Python, pam pam')
-    print('Just kidding, I can also print hearts <3<3<3')
+    manipulate_tables()
 
 
 if __name__ == '__main__':

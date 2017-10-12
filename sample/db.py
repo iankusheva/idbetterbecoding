c5c3ae0c1f00a98896e1f0a242086a4713b3e2e4
iankusheva <irene.ankusheva@gmail.com>
2017-10-12 22:53:11 +0300

    task3 moved db methods to a separate file, added smart.db with structured tables

--- sample/db.py
+++ sample/db.py
@@ -55,25 +55,3 @@ def create_table(dbname):
     cursor.execute('CREATE TABLE table1 ('
                    'userid integer PRIMARY KEY, username text)')
     return dbname
-
-
-@click.command()
-@click.option('--action', prompt='Choose an action to perform (CUD, or Q for quit)', type=click.Choice(['C', 'U', 'D', 'Q']))
-def do_stuff(action):
-    if action == 'C':
-        create_table(standalone_mode=False)
-    elif action == 'U':
-        add_user(standalone_mode=False)
-    elif action == 'D':
-        delete_user(standalone_mode=False)
-    elif action == 'Q':
-        sys.exit(0)
-
-
-def main():
-    while True:
-        do_stuff(standalone_mode=False)
-
-
-if __name__ == '__main__':
-    main()

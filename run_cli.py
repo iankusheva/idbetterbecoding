1e6ee5b059250b04e1488d9984247ea275247e4c
iankusheva <irene.ankusheva@gmail.com>
2017-10-22 20:12:19 +0300

    task4 fixed json passing: from collecting as a click argument to collecting it from file (filename passed as a click option)

--- run_cli.py
+++ run_cli.py
@@ -3,89 +3,6 @@ import json
 from dp_app import db
 
 
-json_string_global = """
-{
-	"users": [{
-		"vasya": {
-			"favourites": [{
-				"book": "Harry Potter and the Prisoner of Azkaban",
-				"author": "J. K. Rowling"
-			},
-			{
-				"book": "Harry Potter and the Deathly Hallows",
-				"author": "J. K. Rowling"
-			},
-			{
-				"book": "Harry Potter and the Half-Blood Prince",
-				"author": "J. K. Rowling"
-			}]
-		}
-	}, {
-		"petya": {
-			"favourites": [{
-				"book": "The Lord of the Rings",
-				"author": "J. R. R. Tolkien"
-			},{
-				"book": "Three Men in a Boat",
-				"author": "Jerome K. Jerome"
-			},{
-				"book": "Ham on Rye",
-				"author": "Charles Bukowski"
-			}]
-		}
-	}, {
-		"vasilisa": {
-			"favourites": [{
-				"book": "Harry Potter and the Prisoner of Azkaban",
-				"author": "J. K. Rowling"
-			},{
-				"book": "The Martian Chronicles",
-				"author": "Ray Bradbury"
-			},{
-				"book": "A Scandal in Bohemia",
-				"author": "Arthur Conan Doyle"
-			}]
-		}
-	}, {
-		"sashka": {
-			"favourites": [{
-				"book": "A Scandal in Bohemia",
-				"author": "Arthur Conan Doyle"
-			},{
-				"book": "The Adventure of the Yellow Face",
-				"author": "Arthur Conan Doyle"
-			},{
-				"book": "The Adventure of the Final Problem",
-				"author": "Arthur Conan Doyle"
-			},{
-				"book": "The Poison Belt",
-				"author": "Arthur Conan Doyle"
-			},{
-				"book": "Ham on Rye",
-				"author": "Charles Bukowski"
-			}]
-		}
-	}, {
-		"gregory": {
-			"favourites": [{
-				"book": "The Lord of the Rings",
-				"author": "J. R. R. Tolkien"
-			},{
-				"book": "Harry Potter and the Prisoner of Azkaban",
-				"author": "J. K. Rowling"
-			},{
-				"book": "Ham on Rye",
-				"author": "Charles Bukowski"
-			}]
-		}
-	}, {
-		"solnyshko35": {
-			"favourites": []
-		}
-	}]
-}
-"""
-
 @click.group()
 def cli():
     pass
@@ -112,11 +29,12 @@ def delete_user_from_db(dbname, username):
 
 
 @cli.command('add_book')
-@click.argument('json_string')
+@click.option('--json_file', default='json_string_books_only.txt')
 @click.option('--dbname', default='favourite_books.db')
-def upload_books_into_db(json_string, dbname):
+def upload_books_into_db(json_file, dbname):
+    json_string = open(db.get_full_path_to_file(json_file)).read()
     parsed_json = json.loads(json_string)
-    path_to_db = db.get_full_path_to_db(dbname)
+    path_to_db = db.get_full_path_to_file(dbname)
 
     with db.DatabaseConnection(path_to_db=path_to_db) as conn:
         cursor = conn.cursor()
@@ -127,11 +45,12 @@ def upload_books_into_db(json_string, dbname):
 
 
 @cli.command('add_user_with_books')
-@click.argument('json_string')
+@click.option('--json_file', default='json_string_users_with_books.txt')
 @click.option('--dbname', default='favourite_books.db')
-def upload_user_with_books_into_db(json_string, dbname):
+def upload_user_with_books_into_db(json_file, dbname):
+    json_string = open(db.get_full_path_to_file(json_file)).read()
     parsed_json = json.loads(json_string)
-    path_to_db = db.get_full_path_to_db(dbname)
+    path_to_db = db.get_full_path_to_file(dbname)
     list_of_users = parsed_json['users']
 
     with db.DatabaseConnection(path_to_db=path_to_db) as conn:

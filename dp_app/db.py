6aa7772c94d96d60bb3df1087ac30a6ebe12d20b
iankusheva <irene.ankusheva@gmail.com>
2017-10-22 20:11:52 +0300

    task4 fixed json passing: from collecting as a click argument to collecting it from file (filename passed as a click option)

--- dp_app/db.py
+++ dp_app/db.py
@@ -34,7 +34,7 @@ def connect_to_db(path_to_db):
     conn.close()
 
 
-def get_full_path_to_db(dbname):
+def get_full_path_to_file(dbname):
     running_dir = os.path.dirname(os.path.abspath(__file__))
     return os.path.join(running_dir, '{}'.format(dbname))
 
@@ -43,46 +43,6 @@ def print_all_tables(path_to_db, conn):
     cursor = conn.cursor()
     for table in ('table_users', 'table_books', 'table_authors', 'table_userbooks'):
         print(table, '-' , cursor.execute('SELECT * FROM {}'.format(table)).fetchall(), '\n')
-
-
-def add_user(dbname, username):
-    path_to_db = get_full_path_to_db(dbname)
-    if not os.path.exists(path_to_db):
-        print('Database {}.db does not exist, can\'t update'.format(dbname))
-        return
-    conn = sqlite3.connect(path_to_db)
-    cursor = conn.cursor()
-    cursor.execute("INSERT INTO table1 VALUES (NULL, ?)", (username,))
-    conn.commit()
-    print('Added username "{}" into {}.db'.format(username, dbname))
-    # print(cursor.execute('SELECT * FROM table1').fetchall())
-
-
-def delete_user(dbname, username):
-    path_to_db = get_full_path_to_db(dbname)
-    if not os.path.exists(path_to_db):
-        print('Database {}.db does not exist, nothing to delete'.format(dbname))
-        return
-    conn = sqlite3.connect(path_to_db)
-    cursor = conn.cursor()
-    if cursor.execute('SELECT * FROM table1 WHERE username=?', (username,)).fetchone():
-        cursor.execute("DELETE FROM table1 WHERE username = ?", (username,))
-        conn.commit()
-        print('Deleted username "{}" from {}.db'.format(username, dbname))
-    else:
-        print('No username {} in database {}, nothing to delete'.format(username, dbname))
-
-
-def create_table(dbname):
-    path_to_db = get_full_path_to_db(dbname)
-    if os.path.exists(path_to_db):
-        print('Database {}.db already exists'.format(dbname))
-        return
-    conn = sqlite3.connect(path_to_db)
-    cursor = conn.cursor()
-    cursor.execute('CREATE TABLE table1 ('
-                   'userid integer PRIMARY KEY, username text UNIQUE)')
-    return dbname
 
 
 def create_tables_in_db(conn):

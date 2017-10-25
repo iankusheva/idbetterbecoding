2c927d4a257a8f1441c6267f41e000e1952dcbae
Irene <irene.ankusheva@gmail.com>
2017-10-25 18:51:51 +0300

    Task4 done (DB data upload via JSON)
    * task4 part1
    
    * task4 made it to work but the structure is terrifying
    
    * task4 slight fixes
    
    * task4 added context manager
    
    * task4 fixed print_all_tables method to work with context manager
    
    * task4 fixed json passing: from collecting as a click argument to collecting it from file (filename passed as a click option)
    
    * task4 fixed json passing: from collecting as a click argument to collecting it from file (filename passed as a click option)
    
    * task4 changed inserting into table queries to more user friendly ones
    
    * task4 added json validation though it is not working lol
    
    * task4 fixed some issues
    
    * task4 fixed json! changed the schema a bit
    
    * task4 fixed things a bit more
    
    * task4 fixed things again

--- dp_app/db.py
+++ dp_app/db.py
@@ -2,55 +2,69 @@ import os
 import sqlite3
 
 
-def get_full_path_to_db(dbname):
+class DatabaseConnection:
+
+    def __init__(self, path_to_db):
+        self.path_to_db = path_to_db
+        self.conn = None
+
+    def __enter__(self):
+        self.conn = sqlite3.connect(self.path_to_db)
+        create_tables_in_db(self.conn)
+        return self.conn
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        try:
+            self.conn.close()
+        except:
+            pass
+
+
+def get_full_path_to_file(dbname):
     running_dir = os.path.dirname(os.path.abspath(__file__))
-    return os.path.join(running_dir, '{}.db'.format(dbname))
+    return os.path.join(running_dir, dbname)
 
 
-def add_user(dbname, username):
-    path_to_db = get_full_path_to_db(dbname)
-    if not os.path.exists(path_to_db):
-        print('Database {}.db does not exist, can\'t update'.format(dbname))
+def print_all_tables(path_to_db, conn):
+    for table in ('table_users', 'table_books', 'table_authors', 'table_userbooks'):
+        print(table, '-' , conn.execute('SELECT * FROM {}'.format(table)).fetchall(), '\n')
+
+
+def create_tables_in_db(conn):
+    with open('/home/irina/Documents/pythonic_stuff/idbetterbecoding/dp_app/create_empty_tables_query.txt') as file_:
+        conn.executescript(file_.read())
+
+
+def upload_book_into_db(bookname, author, path_to_db, conn):
+    # skip the rest if the book already exists
+    if conn.execute("SELECT bookname from table_books WHERE bookname=?", (bookname, )).fetchall():
         return
-    conn = sqlite3.connect(path_to_db)
-    cursor = conn.cursor()
-    cursor.execute("INSERT INTO table1 VALUES (NULL, '{}')".format(username))
+
+    # add author, keep id
+    conn.execute("INSERT OR IGNORE INTO table_authors (authorname) VALUES (?)", (author,))
+    author_id = conn.execute("SELECT authorid from table_authors WHERE authorname=?", (author, )).fetchone()[0]
+
+    # add book with a link to its author
+    conn.execute("INSERT OR IGNORE INTO table_books (bookname, authorid) VALUES (?, ?)", (bookname, author_id))
+
     conn.commit()
-    print('Added username "{}" into {}.db'.format(username, dbname))
-    # print(cursor.execute('SELECT * FROM table1').fetchall())
 
 
-def delete_user(dbname, username):
-    path_to_db = get_full_path_to_db(dbname)
-    if not os.path.exists(path_to_db):
-        print('Database {}.db does not exist, nothing to delete'.format(dbname))
-        return
-    conn = sqlite3.connect(path_to_db)
-    cursor = conn.cursor()
-    if cursor.execute('SELECT * FROM table1 WHERE username="{}"'.format(username)).fetchone():
-        cursor.execute("DELETE FROM table1 WHERE username = '{}'".format(username))
-        conn.commit()
-        print('Deleted username "{}" from {}.db'.format(username, dbname))
-    else:
-        print('No username {} in database {}, nothing to delete'.format(username, dbname))
+def upload_user_into_db(user, path_to_db, conn):
+    username = user["username"]
 
+    conn.execute("INSERT OR IGNORE INTO table_users (username) VALUES (?)", (username,))
+    user_id = conn.execute("SELECT userid from table_users WHERE username=?", (username,)).fetchone()[0]
 
-def create_table(dbname):
-    path_to_db = get_full_path_to_db(dbname)
-    if os.path.exists(path_to_db):
-        print('Database {}.db already exists'.format(dbname))
-        return
-    conn = sqlite3.connect(path_to_db)
-    cursor = conn.cursor()
-    cursor.execute('CREATE TABLE table1 ('
-                   'userid integer PRIMARY KEY, username text UNIQUE)')
-    return dbname
+    for book in user['favourites']:
+        bookname = book['book']
+        author = book['author']
 
+        if not conn.execute("SELECT bookname from table_books WHERE bookname=?", (bookname,)).fetchall():
+            upload_book_into_db(bookname, author, path_to_db, conn)
+        book_id = conn.execute("SELECT bookid from table_books WHERE bookname=?", (bookname,)).fetchone()[0]
 
-def upload_json_into_db(parsed_string):
-    conn = sqlite3.connect('smart.db')
-    cursor = conn.cursor()
-    for elem in parsed_string['books']:
-        bookname = elem['bookname']
-        author = elem['authorname']
-        cursor.execute("INSERT OR IGNORE INTO table_authors VALUES (NULL, ?)", (author,))
+        if not conn.execute("SELECT * from table_userbooks WHERE userid=? AND bookid=?", (user_id, book_id)).fetchall():
+            conn.execute("INSERT OR IGNORE INTO table_userbooks VALUES (?, ?)", (user_id, book_id))
+
+    conn.commit()
--- run_cli.py
+++ run_cli.py
@@ -1,5 +1,19 @@
 import click
+import json
+import jsonschema
 from dp_app import db
+
+
+def validate_json(json_file, json_schema):
+    with open(db.get_full_path_to_file(json_file)) as file_with_string, open(db.get_full_path_to_file(json_schema)) as file_with_schema:
+        try:
+            json_loaded = json.loads(file_with_string.read())
+            jsonschema.validate(json_loaded, json.loads(file_with_schema.read()))
+            return json_loaded
+        except jsonschema.ValidationError as e:
+            print(e.message)
+        except jsonschema.SchemaError as e:
+            print(e)
 
 
 @click.group()
@@ -7,24 +21,33 @@ def cli():
     pass
 
 
-@cli.command('create')
-@click.argument('dbname')
-def create_db(dbname):
-    db.create_table(dbname)
+@cli.command('add_book')
+@click.option('--json_file', default='json_string_books_only.txt')
+@click.option('--dbname', default='favourite_books.db')
+def upload_books_into_db(json_file, dbname):
+    parsed_json = validate_json(json_file, 'books_only_schema.json')
+    path_to_db = db.get_full_path_to_file(dbname)
+
+    with db.DatabaseConnection(path_to_db=path_to_db) as conn:
+        for book in parsed_json['books']:
+            bookname = book['book']
+            author = book['author']
+            db.upload_book_into_db(bookname, author, path_to_db, conn)
+        db.print_all_tables(path_to_db, conn)
 
 
-@cli.command('add')
-@click.argument('dbname')
-@click.argument('username')
-def add_user_to_db(dbname, username):
-    db.add_user(dbname, username)
+@cli.command('add_users_with_books')
+@click.option('--json_file', default='json_string_users_with_books.txt')
+@click.option('--dbname', default='favourite_books.db')
+def upload_user_with_books_into_db(json_file, dbname):
+    parsed_json = validate_json(json_file, 'users_with_books_schema.json')
+    path_to_db = db.get_full_path_to_file(dbname)
+    list_of_users = parsed_json['users']
 
-
-@cli.command('delete')
-@click.argument('dbname')
-@click.argument('username')
-def delete_user_from_db(dbname, username):
-    db.delete_user(dbname, username)
+    with db.DatabaseConnection(path_to_db=path_to_db) as conn:
+        for user in list_of_users:
+            db.upload_user_into_db(user, path_to_db, conn)
+        db.print_all_tables(path_to_db, conn)
 
 
 if __name__ == '__main__':
